6b80523d235260fdcf47cd2459eebddf34f5236e
brookton <davidbrookton@gmail.com>
2019-06-04 08:09:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards() {
+    let names = [ 'Lisa', 'Kaitlin', 'Jan']
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+return newArray
+}
+
+function countdown(number){
+    let i = number
+    do {
+      console.log(i)
+      i -= 1
+    } while (i >= 0)
+}
