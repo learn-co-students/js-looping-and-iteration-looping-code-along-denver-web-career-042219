e3d42df939fcb1dd37ae224eae1eeb68566250f7
drbarq <J.Tustin@gmail.com>
2019-06-04 15:04:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names) {
+    let thankyous = []
+    for (let i = 0; i < names.length; i++) {
+        thankyous.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return thankyous
+}
+
+function countdown(numbers) {
+    while (numbers > 0 ) {
+      console.log(numbers);
+      numbers -= 1;
+    }
+    console.log(numbers);
+  }
+
+
+
+writeCards(names)
+
