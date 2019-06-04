6d344faef0fe0c04ae22f19268441fe7d7756ffa
JosephPR <reilly4181@gmail.com>
2019-06-03 20:43:57 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards() {
+  let peeps = [ 'Lisa', 'Kaitlyn', 'Jan']
+  let s = "surprise"
+  for (let i = 0; i < peeps.length; i++) {
+    return `Thank you, ${peeps[i]}, for the wonderful ${s} gift!`;
+  }
+
+  return peeps;
+}
