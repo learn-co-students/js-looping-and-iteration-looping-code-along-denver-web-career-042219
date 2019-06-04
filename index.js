c77fb5e91bbd6f57858e556211454bdd667a5836
Jessica Pope <jessica.pope.co@gmail.com>
2019-06-04 10:54:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+  const newArray = []
+  for (let i = 0;i < array.length ; i++ ){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArray
+}
+
+function countdown(number){
+    while (number > 0){
+      console.log(number)
+      number -= 1
+    }
+    console.log(number)
+}
