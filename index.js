e30eb4927d70827718b64f88098c591ffe0abefb
Jessica Pope <jessica.pope.co@gmail.com>
2019-10-29 21:14:55 -0600

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// Code your solutions in this file
 function writeCards(array, event){
-  const newArray = []
-  for (let i = 0;i < array.length ; i++ ){
-    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  const writeCardsArray = []
+  for (let i = 0; i < array.length; i++){
+    writeCardsArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
   }
-  return newArray
+  return writeCardsArray
 }
 
-function countdown(number){
-    while (number > 0){
-      console.log(number)
-      number -= 1
-    }
-    console.log(number)
+function countdown(c){
+  let i = c
+  while (i >= 0){
+    console.log(i)
+    i--
+  }
 }
